19d1cfc3853c5d096d15ed3f901f1048c57823d2
Benjamin Li <benji112358@gmail.com>
2020-12-01 20:40:54 -0500

    member count is approx

--- dbl_class.py
+++ dbl_class.py
@@ -43,7 +43,7 @@ class TopGG(commands.Cog):
             print(f"num users: {member_count}")
             # shuffle the bot's presence
             msg_count_presence = f"{metrics_utils.get_msg_count()} msgs"
-            member_count_presence = f"{round(member_count/1000, 2)}k darlings"
+            member_count_presence = f"~{round(member_count/1000, 1)}k darlings"
             presence = random.choice([msg_count_presence, msg_count_presence, member_count_presence])
             await self.bot.change_presence(
                 activity=discord.Activity(type=discord.ActivityType.playing, name=f"in {guild_count} | {presence}"))
